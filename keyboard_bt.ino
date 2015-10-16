3e0c552c51eb92074ae17e4672ebcaf53ac39b31
jake waters <jerkey@g-cipher.net>
2015-10-16 16:02:19 -0700

    it actually works with control keys now

---
+++ keyboard_bt.ino
@@ -0,0 +1,21 @@
+#include<SoftwareSerial.h>
+SoftwareSerial RC(9, 8);// bluetooth TX,RX
+char RCD;
+void setup() {
+  RC.begin(9600);
+  Keyboard.begin();
+}
+
+void loop() {
+  if (RC.available() > 0) {
+    RCD = RC.read();//recieve value from serial
+    RC.println((byte)RCD);
+    if (RCD < 26) {
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(RCD+96);
+      Keyboard.releaseAll();
+    } else {
+      Keyboard.write(RCD);//print char as recieved byte by byte
+    }
+  }
+}
