a0715b623f5ab5c780f5c6e50ca39f8a93446cd7
jake waters <jerkey@g-cipher.net>
2015-10-17 19:54:01 -0700

    edit comments

--- serial_keyboard.ino
+++ serial_keyboard.ino
@@ -1,5 +1,17 @@
+// serial_keyboard.ino for emulating a keyboard with serial input
+// use it when you forgot a keyboard, or need one computer to type
+// into another.  Or use it with a serial bluetooth dongle.
+//
+// originally keyboard_bt.ino
+// by http://github.com/jerkey
+// from http://www.instructables.com/id/Using-Arduino-Leonardo-as-mouse-and-keyboard-contr/
+// (unfortunately the program above does not work with enter, backspace, etc :)
+// see also:
+// http://joshfire-tech.tumblr.com/post/65032568887/arduino-keyboard-emulation-send-real-key-codes
+// https://www.arduino.cc/en/Reference/KeyboardModifiers
+
 #include<SoftwareSerial.h>
-SoftwareSerial RC(9, 8);// bluetooth TX,RX
+SoftwareSerial RC(9, 8);// serial TX,RX on PB5, PB4
 char RCD;
 void setup() {
   RC.begin(9600);
@@ -8,14 +20,14 @@ void setup() {
 
 void loop() {
   if (RC.available() > 0) {
-    RCD = RC.read();//recieve value from serial
-    RC.println((byte)RCD);
-    if (RCD <= 27) {
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(RCD+96);
+    RCD = RC.read();
+    RC.println((byte)RCD); //serial print the ASCII code as recieved, byte by byte
+    if (RCD <= 27) { // if user typed a control character
+      Keyboard.press(KEY_LEFT_CTRL); // press control
+      Keyboard.press(RCD+96); // and then the corresponding letter
       Keyboard.releaseAll();
-    } else {
-      Keyboard.write(RCD);//print char as recieved byte by byte
+    } else { // otherwise just send the key pressed
+      Keyboard.write(RCD);
     }
   }
 }
